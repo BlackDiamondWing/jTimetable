94728736f34c54ee74d2488aa5ccd8d59254dde2
Loreen <loreen.roose@web.de>
2022-07-20 20:58:47 +0200

    Die Run Configurationen gehen endlich :D

--- src/de/bremen/jTimetable/gui/MenuController.java
+++ src/de/bremen/jTimetable/gui/MenuController.java
@@ -1,10 +1,15 @@
 package de.bremen.jTimetable.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,8 +19,32 @@ public class MenuController implements Initializable {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    private Button btn_switchToSettings;
+    @FXML
+    private Button btn_showTimetable;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
+
+    @FXML
+    private void handle_showTimetable(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Timetable.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Stundenplan");
+        //TODO Datum auswerten, richtigen Stundenplan anzeigen
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handle_switchToSettings(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Settings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Einstellungen");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
