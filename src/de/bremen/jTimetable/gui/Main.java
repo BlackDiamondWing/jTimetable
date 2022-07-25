fb923edbee7da3d081273c6548f4bae2421c2e01
BlackDiamondWing <loreen.roose@web.de>
2022-07-25 15:32:20 +0200

    Weiterarbeit gui

--- src/de/bremen/jTimetable/gui/Main.java
+++ src/de/bremen/jTimetable/gui/Main.java
@@ -2,10 +2,7 @@ package de.bremen.jTimetable.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //breite x höhe
+        Scene scene = new Scene(fxmlLoader.load(), 200, 300);
         stage.setTitle("Menü Stundenplansoftware");
         stage.setScene(scene);
         stage.show();
--- src/de/bremen/jTimetable/gui/MenuController.java
+++ src/de/bremen/jTimetable/gui/MenuController.java
@@ -4,9 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,25 +18,54 @@ import java.util.ResourceBundle;
 
 public class MenuController implements Initializable {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+    /**
+     * Container.
+     */
 
+    /**
+     * DatePicker.
+     */
+    @FXML
+    public DatePicker dtpckr_selectDateForTimetable;
+
+    /**
+     * Buttons.
+     */
     @FXML
     private Button btn_switchToSettings;
     @FXML
     private Button btn_showTimetable;
+    @FXML
+    public MenuButton mnbtn_selectTimetableFor;
+    @FXML
+    public MenuButton mnbtn_typeOfObject;
+
+    /**
+     * Labels.
+     */
+    @FXML
+    public Label lbl_selectTimetableFor;
+    @FXML
+    public Label lbl_typeOfObject;
+    @FXML
+    public Label lbl_mistakeMessage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        mnbtn_selectTimetableFor.setDisable(true);
+        lbl_selectTimetableFor.setDisable(true);
+        dtpckr_selectDateForTimetable.setDisable(true);
+        btn_showTimetable.setDisable(true);
+        lbl_mistakeMessage.setDisable(true);
     }
 
     @FXML
     private void handle_showTimetable(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Timetable.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Stundenplan");
-        //TODO Datum auswerten, richtigen Stundenplan anzeigen
+        //TODO Datum auswerten, richtigen Stundenplan anzeigen (in dessen initialise? oder doch zusammen in controller?)
         stage.setScene(scene);
         stage.show();
     }
@@ -41,10 +73,19 @@ public class MenuController implements Initializable {
     @FXML
     private void handle_switchToSettings(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Settings.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Einstellungen");
         stage.setScene(scene);
         stage.show();
     }
 
+    public void handle_typeOfObject(ActionEvent actionEvent) {
+        //Lade alle Möglichen Objektarten: Raum, Dozent, Verbindungsobjekt zwischen Lehrgang und Studienabschnitt
+        //Schreibe die ausgewählte Objektart ins Label
+        //enable nächsten abschnitt
+    }
+
+    public void handle_selectTimetableFor(ActionEvent actionEvent) {
+    }
 }
